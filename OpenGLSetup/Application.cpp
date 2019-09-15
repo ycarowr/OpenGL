2f07dd72d97beb0f8ad9aa519d5bb4770866b9ec
ycarowr <ywrycaro@gmail.com>
2019-09-15 21:51:08 +0300

    First draw call with modern open gl

--- OpenGLSetup/Application.cpp
+++ OpenGLSetup/Application.cpp
@@ -1,8 +1,10 @@
+//An OpenGL documentation: http://docs.gl/
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include<iostream>
 
-void drawTriangle()
+void DrawTriangleWithLegacyOGL()
 {
 	//a triangle
 	glBegin(GL_TRIANGLES);
@@ -10,6 +12,54 @@ void drawTriangle()
 	glVertex2f(0, 0.5f);
 	glVertex2f(0.5f, -0.5f);
 	glEnd();
+}
+
+void Start()
+{
+	//declaring how many buffers we want
+	const int amount = 1;
+
+	//declare the buffer, its basically an id or the 
+	//address where the buffer is stored in memory
+	unsigned int buffer;
+
+	//send the buffer to opengl in order it to allocate the buffer properly
+	glGenBuffers(amount, &buffer);
+
+	//tells the opengl how we are gonna use the buffer, this case, as a simple array
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+
+	//declare the data we want to put in the buffer
+	float trianglePositions [] 
+	{
+		-0.5f, -0.5f, 
+		0, 0.5f, 
+		0.5f, -0.5f
+	};
+
+	//define the size of the buffer (in bytes), check the documentation
+	const int size = 6 * sizeof(float); // sizeof(trianglePositions);
+
+	//the usage is a hint to opengl of how we are gonna use the it, check documentation
+	auto usage = GL_STATIC_DRAW;
+
+	//finally send the data 
+	glBufferData(GL_ARRAY_BUFFER, size, trianglePositions, usage);
+}
+
+void Update()
+{
+	//type of stuff we wanna draw
+	auto type = GL_TRIANGLES;
+
+	//we want to draw from the beginning
+	const int begin = 0;
+
+	//all the way up to the end of the array
+	const int count = 3;
+	
+	//a draw call!
+	glDrawArrays(type, begin, count);
 }
 
 int main(void)
@@ -37,13 +87,16 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
+	//before loop
+	Start();
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		drawTriangle();
+		Update();
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
