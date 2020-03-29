fffc851457c3960e10cb410a14feac2d1857228d
Ycaro Weschenfelder Rodrigues <ywrycaro@gmail.com>
2020-03-29 15:49:11 +0300

    Hello Window working
    https://learnopengl.com/Getting-started/Hello-Window

--- LearnOpenGLTutorials/OpenGL/VS/OpenGL/Teest.cpp
+++ LearnOpenGLTutorials/OpenGL/VS/OpenGL/Teest.cpp
@@ -1,7 +1,69 @@
-#include <GLAD/glad/glad.h>
-#include <GLFW\glfw3.h>
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <random>
+#include <chrono>
+#include <thread> 
 
-void main()
+
+void processInput(GLFWwindow *window)
 {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+int GetRandom()
+{
+	return rand();
+}
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		int isEven = GetRandom() % 2;
+		std::cout << "Random " << isEven << std::endl;
+		if(isEven == 1)
+			glClearColor(1, 0, 0, 1.0f);
+		else 
+			glClearColor(0, 1, 0, 1.0f);
+		
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+
+		processInput(window);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	return 0;
 }
