0676b7f00c3747edd16fc52bfe9edff2c6fb4895
Ycaro Weschenfelder Rodrigues <ywrycaro@gmail.com>
2020-03-29 16:40:09 +0300

    Small refact on render loop

--- LearnOpenGLTutorials/OpenGL/VS/OpenGL/Main.cpp
+++ LearnOpenGLTutorials/OpenGL/VS/OpenGL/Main.cpp
@@ -5,6 +5,8 @@
 #include <chrono>
 #include <thread> 
 
+GLFWwindow* window;
+
 
 void processInput(GLFWwindow *window)
 {
@@ -22,48 +24,73 @@ int GetRandom()
 	return rand();
 }
 
-int main()
+void InitializeOpenGL()
 {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+}
+
+void InitializeWindow()
+{
+	const char* title = "LearnOpenGL";
+	const int width = 800;
+	const int height = 600;
+	window = glfwCreateWindow(width, height, title, NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
-		return -1;
 	}
 	glfwMakeContextCurrent(window);
+}
 
+void InitializeGlad()
+{
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
 	}
+}
 
+void InitializeCallbacks()
+{
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+}
 
+//Loop
+void OnRender()
+{
+	int isEven = GetRandom() % 2;
+	std::cout << "Random " << isEven << std::endl;
+	if (isEven == 1)
+		glClearColor(1, 0, 0, 1.0f);
+	else
+		glClearColor(0, 1, 0, 1.0f);
 
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+}
+
+void RenderLoop()
+{
 	while (!glfwWindowShouldClose(window))
 	{
-		int isEven = GetRandom() % 2;
-		std::cout << "Random " << isEven << std::endl;
-		if(isEven == 1)
-			glClearColor(1, 0, 0, 1.0f);
-		else 
-			glClearColor(0, 1, 0, 1.0f);
-		
-		glClear(GL_COLOR_BUFFER_BIT);
-		
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-
+		OnRender();
 		processInput(window);
-
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+}
+
+int main()
+{
+	InitializeOpenGL();
+	InitializeWindow();
+	InitializeGlad();
+	InitializeCallbacks();
+	RenderLoop();
 	return 0;
 }
